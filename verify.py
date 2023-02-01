35bba4296193546d1f79b8dc32eab6ee4018335d
Blattoid <danielbridgewater87@gmail.com>
2023-02-01 17:27:30 +0000

    Vastly improve integrity check script

--- verify.py
+++ verify.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
+import argparse
+import os
 from PIL import Image
-from os import chdir, listdir
 
-chdir("download")
+parser = argparse.ArgumentParser(
+  prog = os.path.basename(__file__),
+  description = "Checks all downloaded images to make sure there aren't any corrupted files"
+)
 
-for dir in [d for d in listdir() if "." not in d]:
-	for file in listdir(dir):
-		image = Image.open(f"{dir}/{file}")
-		try:
-			image.verify()
-		except:
-			print(f"BAD FILE {dir}/{file}")
+parser.add_argument("-d", "--directory",
+  dest="directory", default="download", help="Download directory to scan")
+parser.add_argument("-v", "--verbose",
+  dest="verbose", action="store_true", help="Include verified files in output")
+args = parser.parse_args()
+
+os.chdir(args.directory)
+
+for chapter in [d for d in os.listdir() if "." not in d]:
+  for file in os.listdir(chapter):
+    path = "/".join((chapter,file))
+
+    try:
+      image = Image.open(path)
+      image.verify()
+    except OSError:
+      print(f"TRUNCATED FILE {path}")
+    except:
+      print(f"BAD FILE {path}")
+    else:
+      if args.verbose:
+        print(f"OK FILE {path}")
